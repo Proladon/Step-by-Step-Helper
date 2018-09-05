3e62e1a7b889fe6b70c68ef32a5537b94c851271
Proladon <Proladon@gmail.com>
2018-09-05 20:36:41 +0800

    first upload

---
+++ Code/App.py
@@ -0,0 +1,20 @@
+import sys
+from PyQt5.QtWidgets import QDialog, QApplication
+from PyQt5 import QtCore, QtGui
+from N import Ui_MainWin
+
+
+class App(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWin()
+        self.ui.setupUi(self)
+        self.show()  
+
+        
+        
+
+app = QApplication(sys.argv)
+a = App()
+a.show()
+sys.exit(app.exec_())
