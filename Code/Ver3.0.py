d929e3609b77dadcb06726ce51076e67346e7037
Proladon <Proladon@gmail.com>
2019-03-18 23:20:27 +0800

    topmost

--- Code/Ver3.0.py
+++ Code/Ver3.0.py
@@ -1,35 +1,33 @@
 from tkinter import*
 
 
-class Top1:
+class Mainwin:
 	def __init__(self, master):
 		self.master = master
+		master.attributes("-topmost",1)
 		master.geometry("400x200+700+400")
 		master.title("Classes Tkinter GUI")
 		
-		self.quit_btn = Button(text="Close", command=master.quit)
+
+		self.quit_btn = Button(text="bruh", command=self.call_diaolog)
 		self.quit_btn.pack()
 
-		self.quit_btn = Button(text="bruh", command=self.wee)
-		self.quit_btn.pack()
-
-	def wee(self):
-		class Top2:
+	def call_diaolog(self):
+		class step_creater:
 			def __init__(self, master2):
 				self.master = master2
-				master2.geometry("400x200+700+400")
+				master2.geometry("400x600+400+400")
 				master2.title("Classes Tkinter GUI")
 				
 				self.quit_btn = Button(master2, text="Close", command=self.close)
 				self.quit_btn.pack()
 			def close(self):
 				root2.destroy()
-
 		root2 = Tk()
-		win = Top2(root2)
+		win = step_creater(root2)
 		root2.mainloop()
 
 
 root = Tk()
-win = Top1(root)
+win = Mainwin(root)
 root.mainloop()
